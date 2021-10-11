e084b7684360470eb0c7793eb9ae0deb4adab1cd
Daniel Pratama <danielaquaries@gmail.com>
2021-10-11 16:57:01 +0700

    Menambah Coding Binding di Main Activity.

--- app/src/main/java/com/example/counterapplication/MainActivity.kt
+++ app/src/main/java/com/example/counterapplication/MainActivity.kt
@@ -3,19 +3,21 @@ package com.example.counterapplication
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.counterapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModels: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.viewModel = viewModels
 
     }
 }
